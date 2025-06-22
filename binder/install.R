9fb6359874b8b8c350d4328ef567f9988f6d6604
lorraine.saju <lorraine.developer@gmail.com>
2025-06-22 07:54:34 +0200

    Complete R analysis, add Binder configuration, and plots

--- binder/install.R
+++ binder/install.R
@@ -1,11 +1,1 @@
-# install.R
-# This script is run by Binder to install R packages
-
-install.packages(c(
-  "tidyverse", # Includes dplyr, ggplot2, readr etc.
-  "devtools"   # Often useful for installing packages from GitHub, etc.
-), repos = "[http://cran.us.r-project.org](http://cran.us.r-project.org)")
-
-# Ensure all dependencies are available if not part of tidyverse
-# For example, if you specifically needed 'readr' and not just 'tidyverse'
-# install.packages("readr", repos = "[http://cran.us.r-project.org](http://cran.us.r-project.org)")
+install.packages(c("tidyverse", "knitr", "viridis", "RColorBrewer"), repos = "https://cran.microsoft.com/snapshot/2024-05-31/")
